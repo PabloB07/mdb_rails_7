fa034747b3e8f77df0fd148c2b66e4532cef006e
pablob <pabloblanco0798@gmail.com>
2022-11-10 10:34:35 -0300

    MDB in Rails 7

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "mdb-ui-kit", to: "https://ga.jspm.io/npm:mdb-ui-kit@6.0.0/js/mdb.min.js", preload: true
